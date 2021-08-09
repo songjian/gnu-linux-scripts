c1420a9ec46949b231ae362a6c7394b6792493d4
宋健 <songjian@codeorder.cn>
2021-08-09 10:37:07 +0800

    修改python安装脚本

--- install_python.sh
+++ install_python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install -y python3 python3-pip
+. ~/.profile
 . ~/.bashrc
 . ~/.bash_profile
 pip install -i https://mirrors.ustc.edu.cn/pypi/web/simple pip -U
 pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
 pip install --upgrade pip
-pip install jupyter
