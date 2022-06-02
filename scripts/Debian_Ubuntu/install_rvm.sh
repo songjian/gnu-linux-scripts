3f53e3643b9801dff5ca5da2fe60ed41a48e94f4
sj <songjian@codeorder.cn>
2022-06-02 17:52:34 +0800

    优化rvm安装脚本

--- scripts/Debian_Ubuntu/install_rvm.sh
+++ scripts/Debian_Ubuntu/install_rvm.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+
+
+:' 国内
 git clone git@github.com:rvm/rvm.git /tmp/rvm
 /tmp/rvm/binscripts/rvm-installer
 . ~/.bashrc
 . ~/.bash_profile
 mkdir ~/.rvm/user/
 echo "ruby_url=https://cache.ruby-china.com/pub/ruby" > ~/.rvm/user/db
+'
+
 rvm install 2.7.2 --disable-binary
 rvm use 2.7.2 --default
 gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
