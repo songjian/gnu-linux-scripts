7da23e6b84d4b0b017cda880ad558212e421d742
sj <songjian@codeorder.cn>
2022-06-02 17:45:09 +0800

    修改安装rvm脚本

--- scripts/Debian_Ubuntu/install_rvm.sh
+++ scripts/Debian_Ubuntu/install_rvm.sh
@@ -3,6 +3,7 @@ git clone git@github.com:rvm/rvm.git /tmp/rvm
 /tmp/rvm/binscripts/rvm-installer
 . ~/.bashrc
 . ~/.bash_profile
+mkdir ~/.rvm/user/
 echo "ruby_url=https://cache.ruby-china.com/pub/ruby" > ~/.rvm/user/db
 rvm install 2.7.2 --disable-binary
 rvm use 2.7.2 --default
