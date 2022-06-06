ffdec2bec6a03a53a3d93e3b1a0f4aaa74c1ad72
sj <songjian@codeorder.cn>
2022-06-06 13:51:07 +0800

    修改脚本

--- scripts/Debian_Ubuntu/install_phpenv_php-build.sh
+++ scripts/Debian_Ubuntu/install_phpenv_php-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone git://github.com/phpenv/phpenv.git ~/.phpenv
+git clone https://github.com/phpenv/phpenv.git ~/.phpenv
 echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(phpenv init -)"' >> ~/.bash_profile
 exec $SHELL -l
