628672bf865d908ace2f062939ce7397ecee7a67
sj <songjian@codeorder.cn>
2022-06-06 13:57:11 +0800

    更新脚本

--- scripts/Debian_Ubuntu/install_phpenv_php-build.sh
+++ scripts/Debian_Ubuntu/install_phpenv_php-build.sh
@@ -3,4 +3,4 @@ git clone https://github.com/phpenv/phpenv.git ~/.phpenv
 echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(phpenv init -)"' >> ~/.bash_profile
 exec $SHELL -l
-git clone git://github.com/php-build/php-build $(phpenv root)/plugins/php-build
+git clone https://github.com/php-build/php-build $(phpenv root)/plugins/php-build
