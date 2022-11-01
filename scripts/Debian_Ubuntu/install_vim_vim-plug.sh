c677cf86b9bd3e23f2f792bbf0268dba5fb023e3
sj <songjian@codeorder.cn>
2022-11-01 16:34:35 +0800

    修改安装vim和vimplug脚本

--- scripts/Debian_Ubuntu/install_vim_vim-plug.sh
+++ scripts/Debian_Ubuntu/install_vim_vim-plug.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 sudo apt update
 sudo apt install -y vim
-git clone git@github.com:junegunn/vim-plug.git /tmp/vim-plug
 mkdir -p ~/.vim/autoload
-cp /tmp/vim-plug/plug.vim ~/.vim/autoload/plug.vim 
+git clone git@github.com:junegunn/vim-plug.git ~/.vim/autoload/plug.vim
